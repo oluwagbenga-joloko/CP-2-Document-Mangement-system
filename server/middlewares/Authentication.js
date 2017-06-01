105e157e9a02ada607c501aa5c2c09a714bf29d3
Andela Development <oluwagbenga.joloko@andela.com>
2017-06-01 21:13:28 +0100

    [146441835] updated cofig.js

---
+++ server/middlewares/Authentication.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
+
+const authentication = {
+  verifyUser(req, res, next) {
+    const token = req.headers['x-access-token'] || req.headers.authorization || null;
+    if (!token) {
+      res.status(401).send({
+        success: false,
+        msg: 'No token provided'
+      });
+    } else {
+      jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+          res.status(401).send({ success: false, msg: 'invalid token' });
+        }
+        if (decoded) {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    }
+  },
+  checkAdmin(req, res, next) {
+    if (req.decoded.roleId === 1) {
+      next();
+    } else {
+      res.status(401).send({ success: false, msg: 'unauthorized' });
+    }
+  }
+
+};
+export default authentication;
